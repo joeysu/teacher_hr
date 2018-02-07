94ae9a1160840a11e2adbc254b2b036549bca0c3
sujy <joey.su@outlook.com>
2018-02-08 00:11:04 +0800

    refactor to classes style

--- main.py
+++ main.py
@@ -1,42 +1,59 @@
-import os
 import re
 import requests
 
-def main():
-    url = 'http://www.jiaoshizhaopin.net/guangxi/nanning'
-    headers = {'user-agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'}
+class HR(object):
+
+    def __init__(self, url, headers):
+        self.url = url
+        self.headers = headers
     
-    r = requests.get(url, headers=headers)
-    
-    re_page_num = re.compile('<li><a  href=.*</a></li><li class="page_all">.*/(.*)页</li><div class="clear"></div>')
-    m_page_num = re_page_num.findall(r.text)
+    def get_requests(self):
+        return requests.get(url, headers=self.headers)
 
-    payload = {'page': '1'}
-    parse_page(url, payload)
-    return
+    def findall(self, pattern):
+        return re.compile(pattern).findall(self.response.text)
 
-if __name__ == '__main__':
-    main()
 
-    #url = 'http://www.jiaoshizhaopin.net/guangxi/nanning'
-    #headers = {'user-agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'}
+class TeacherHR(HR):
 
-    #r = requests.get(url, headers=headers)
-    #print(r.url)
+    def __init__(self, url, headers, parse_type):
+        super().__init__(url, headers)
+        self.parse_type = parse_type
+        self.params = {}
 
-    #re_city = re.compile('<span class="district_name">(.*)</span>')
-    #m_city = re_city.findall(r.text)
+    def get_requests(self):
+        return requests.get(url, headers=self.headers, params=self.params)
 
-    #re_info = re.compile(r'<h2><a class="title" href="(.*)" target="_blank" title="(.*)">.*')
-    #m_info = re_info.findall(r.text)
+    def run(self):
+        self.response = self.get_requests()
+        self.iters = self.get_iters()
 
-    #r_course = re.compile(r'<li><a title=".*" href="(.*)">(.*)（<span>(.*)</span>）</a></li>')
-    #m_course = r_course.findall(r.text)
+        for iter in range(self.iters):
+            self.params['page'] = str(iter+1)
+            self.response = self.get_requests()
+            getattr(self, 'parse_{}'.format(self.parse_type))()
+            getattr(self, 'process_{}'.format(self.parse_type))()
 
-    #re_date = re.compile(r'<span class="date"><i></i>(.*)</span>')
-    #m_date = re_date.findall(r.text)
+    def get_iters(self):
+         return getattr(self, 'get_iters_{}'.format(self.parse_type))()
 
-    #re_loc = re.compile(r'<a target="_blank" title=".*" >(.*)</a>')
-    #m_loc = re_loc.findall(r.text)
+    def get_iters_0(self):
+        return int(self.findall(r'href=.*</a></li><li class="page_all">.*/(.*)页</li><div class="clear"></div>')[0])
 
-    #print()
+    def parse_0(self):
+        self.city = self.findall(r'<span class="district_name">(.*)</span>')
+        self.info = self.findall(r'<h2><a class="title" href="(.*)" target="_blank" title="(.*)">.*')
+        self.course = self.findall(r'<li><a title=".*" href="(.*)">(.*)（<span>(.*)</span>）</a></li>')
+        self.date = self.findall(r'<span class="date"><i></i>(.*)</span>')
+        self.loc = self.findall(r'<a target="_blank" title=".*" >(.*)</a>')
+
+    def process_0(self):
+        print(self.date)
+
+url = 'http://www.jiaoshizhaopin.net/guangxi/nanning'
+headers = {'user-agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'}
+parse_type = 0
+
+t_0 = TeacherHR(url, headers, parse_type)
+
+t_0.run()
